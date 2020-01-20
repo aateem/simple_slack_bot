229f2b8207e60df2ad619adcd40a7f3e85ebe39b
Artem Roma <fuzzy.finder@gmail.com>
2020-01-20 17:41:50 +0200

    Add user notifying functionality
    * add duplication of user configuration to the config storage - needed
    for optimal update condition checking
    * add process_chat_message event handler, which notifies users in DM

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 from flask import Flask, request, jsonify
 from slack import WebClient
 
-from process_text import get_app_message, split_message, HELP_MESSAGE, ACK_MESSAGE
+from process_text import get_app_message, split_message, HELP_MESSAGE, ACK_MESSAGE, QUOTE_PREFIX
 
 logging.basicConfig(level=os.environ.get("APP_LOGGING_LEVEL", logging.INFO))
 
 app = Flask(__name__)
 
-USER_CONFIG = {}
+USER_CONFIG = {"users": {}, "channels": {}}
 
 
 # debug
@@ -36,13 +36,64 @@ def event_listener():
     if data.get("type") == "event_callback":
         event = data.get("event", {})
 
-        if accept_event(event):
+        if message_for_app(event):
             process_app_message(event)
+
+        if chat_message(event):
+            process_chat_message(event)
 
     return ("", 200)
 
 
-def accept_event(event):
+def chat_message(event):
+    return event.get("type") == "message" and event.get("channel_type") == "channel"
+
+
+def process_chat_message(event):
+    channel = event.get("channel")
+    if channel in USER_CONFIG["channels"]:
+        channel_conf = USER_CONFIG["channels"][channel]
+        phrases_conf = channel_conf.get("phrases", {})
+
+        for phrase in phrases_conf.keys():
+            if phrase in event.get("text", ""):
+                _notify_users(
+                    user_ids=phrases_conf[phrase],
+                    channel_long_id=channel_conf["long_id"],
+                    phrase=phrase,
+                )
+
+
+def _notify_users(user_ids, channel_long_id, phrase):
+    msg = f"""
+Phrase:\n
+{QUOTE_PREFIX}{phrase}\n
+has appeared in {channel_long_id}
+"""
+    for uid in user_ids:
+        dm_channel = _get_dm_channel_id(uid)
+        chat_post_message(channel=dm_channel, message=msg)
+
+
+def _get_dm_channel_id(user_id):
+    dm_channel_id = USER_CONFIG["users"][user_id].get("dm_channel_id")
+    if not dm_channel_id:
+        web_client = WebClient(token=os.environ["SLACK_API_TOKEN"])
+        response = web_client.conversations_open(users=[user_id])
+        if not response.get("ok", False):
+            logging.error(
+                "Failed to open DM channel with user {}. "
+                "Returned error: {}".format(user_id, response.get("error"))
+            )
+            return
+
+        dm_channel_id = response.get("channel", {}).get("id")
+        USER_CONFIG["users"][user_id]["dm_channel_id"] = dm_channel_id
+
+    return dm_channel_id
+
+
+def message_for_app(event):
     event_type = event.get("type")
     return event_type == "app_mention" or (
         event_type == "message"
@@ -96,7 +147,14 @@ def update_user_config(event, phrases, channels):
         logging.error("User is not present in the message description, cannot set the config")
         return
 
-    USER_CONFIG[user] = {"phrases": phrases, "channels": channels}
+    USER_CONFIG["users"][user] = {"phrases": phrases, "channels": channels}
+
+    for chan in channels:
+        chan_id = chan.split("|")[0].strip("<").strip("#")
+        conf_channel = USER_CONFIG["channels"].setdefault(chan_id, {"long_id": chan, "phrases": {}})
+        for phrase in phrases:
+            conf_phrase = conf_channel["phrases"].setdefault(phrase.strip(QUOTE_PREFIX), [])
+            conf_phrase.append(user)
 
     msg = ACK_MESSAGE.format("\n".join(phrase for phrase in phrases), " ".join(channels))
     chat_post_message(event.get("channel"), msg)
